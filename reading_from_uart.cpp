b29b2ed73e6083d7bf22a76e7b71ad0ac211a956
Elad-shmuelii <166764132+Elad-shmuelii@users.noreply.github.com>
2024-04-17 10:42:44 +0300

    Create reading_from_uart.cpp

---
+++ reading_from_uart.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <sys/ioctl.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <thread>
+// C library headers
+// Linux headers
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <errno.h> // Error integer and strerror() function
+#include <termios.h> // Contains POSIX terminal control definitions
+
+void reading_uart_computer() {
+
+  int serial_port = open("/dev/ttyTHS0", O_RDWR);
+  static char read_buf [1];
+  if (serial_port <= 0) {
+        cout << "Error while opening device... " << "errno = " << errno << endl;
+        perror("Something went wrong with open()");
+        //exit(1);
+  }
+  // Create new termios struct, we call it 'tty' for convention
+  struct termios tty;
+
+  // Read in existing settings, and handle any error
+  if(tcgetattr(serial_port, &tty) != 0) {
+      printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+  }
+
+  tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+  tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+  tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
+  tty.c_cflag |= CS8; // 8 bits per byte (most common)
+  tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+  tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+  tty.c_lflag &= ~ICANON;
+  tty.c_lflag &= ~ECHO; // Disable echo
+  tty.c_lflag &= ~ECHOE; // Disable erasure
+  tty.c_lflag &= ~ECHONL; // Disable new-line echo
+  tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+  tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+  tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+
+  tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+  tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+  // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
+  // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
+
+  tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+  tty.c_cc[VMIN] = 40;
+
+  // Set in/out baud rate to be 9600
+  cfsetispeed(&tty, B115200);   
+  cfsetospeed(&tty, B115200);
+
+  // Save tty settings, also checking for error
+  if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+      printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+  }
+  memset(&read_buf, '\0', sizeof(read_buf));
+  
+  while (!close_program){
+
+      memset(&read_buf, '\0', sizeof(read_buf));
+      num_bytes = read(serial_port_gimbal, &read_buf, sizeof(read_buf));
+      cout<<read_buf<<endl;
+      total_string="test message to send";
+      write(serial_port, total_string.c_str(), total_string.length());
+  }
+close(serial_port);
+
+
+  
